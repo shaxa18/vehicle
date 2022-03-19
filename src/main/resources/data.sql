15b0dac3c6fb53b27aa8364bca7b947c7561e839
imomo <imomovshoh21@gmail.com>
2022-03-19 10:28:43 +0500

    first steps

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+
+insert into Country value(2, 'Washington DC', 'USA' , 'North America','United State','American');
+insert into Country value(3, 'Abuja', 'NGN' , 'Africa','Nigeria','American');
+insert into Country value(4, ' New Delhi', 'IN' , 'North America','United State','American');
+insert into Country value(5, 'Budapest', 'HUN' , 'North America','United State','American');
