2a4ee92f8a3528f3526600c0fdc9bac99f53ee32
imomo <imomovshoh21@gmail.com>
2022-03-26 09:35:32 +0500

    contacts/contactsAdd/contactsEdit/contactsDelete functions added

--- src/main/java/com/napa/vehicle/parameters/repositories/LocationRepository.java
+++ src/main/java/com/napa/vehicle/parameters/repositories/LocationRepository.java
@@ -2,6 +2,8 @@ package com.napa.vehicle.parameters.repositories;
 
 import com.napa.vehicle.parameters.models.Location;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LocationRepository extends CrudRepository<Location,Integer> {
 }
