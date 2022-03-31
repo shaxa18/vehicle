caf22d6ffb5365035d78f1166bf7fc59df389b87
imomo <imomovshoh21@gmail.com>
2022-03-31 17:22:39 +0500

    some changes have

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-
-insert into Country value(2, 'Washington DC', 'USA' , 'North America','United State','American');
-insert into Country value(3, 'Abuja', 'NGN' , 'Africa','Nigeria','American');
-insert into Country value(4, ' New Delhi', 'IN' , 'North America','United State','American');
-insert into Country value(5, 'Budapest', 'HUN' , 'North America','United State','American');
