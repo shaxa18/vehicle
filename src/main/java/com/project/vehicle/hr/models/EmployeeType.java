e74365c50602c39f5429b8be11edb7547d317c41
imomo <imomovshoh21@gmail.com>
2022-06-18 22:36:32 +0500

    some packages changed

---
+++ src/main/java/com/project/vehicle/hr/models/EmployeeType.java
@@ -0,0 +1,17 @@
+package com.project.vehicle.hr.models;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.project.vehicle.parameters.models.CommonObject;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper=false)
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class EmployeeType extends CommonObject {
+
+}
